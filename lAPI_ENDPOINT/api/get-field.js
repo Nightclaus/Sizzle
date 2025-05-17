0e94ebe12540d6628be959542019d880787163dc
Nightclaus <159602138+Nightclaus@users.noreply.github.com>
2025-05-17 21:40:34 +1000

    Auto Commit Message

--- lAPI_ENDPOINT/api/get-field.js
+++ lAPI_ENDPOINT/api/get-field.js
@@ -15,10 +15,6 @@ if (!admin.apps.length) {
 const db = admin.firestore();
 
 export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
-
   let rawBody = '';
 
   req.on('data', (chunk) => {
